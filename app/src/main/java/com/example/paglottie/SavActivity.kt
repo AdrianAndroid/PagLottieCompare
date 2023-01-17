ff52b7138188c7e17929a06c214e098d595d374e
zhaojian <zhaojian@apusapps.com>
2023-01-17 21:04:17 +0800

    仅留lottie

--- app/src/main/java/com/example/paglottie/SavActivity.kt
+++ app/src/main/java/com/example/paglottie/SavActivity.kt
@@ -4,24 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
-import org.libpag.PAGFile
-import org.libpag.PAGView
 
 class SavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_big)
+        setContentView(R.layout.activity_sav)
 
         findViewById<View>(R.id.btn1).setOnClickListener {
-            val pagContainer = findViewById<FrameLayout>(R.id.pagContainer);
-            val pagFile = PAGFile.Load(assets, "sav/pag/shadu.pag")
-            val pagView = PAGView(this@SavActivity)
-            pagContainer.addView(pagView)
-            pagView.composition = pagFile
-            pagView.setRepeatCount(10000)
-            it.post {
-                pagView.play()
-            }
         }
 
         findViewById<View>(R.id.btn2).setOnClickListener {
@@ -35,18 +24,5 @@ class SavActivity : AppCompatActivity() {
 
 
     fun dynamicAdd() {
-        // ViewGroup
-        val pagContainer = findViewById<FrameLayout>(R.id.pagContainer);
-        // 加载File
-        val pagFile = PAGFile.Load(assets, "sav/pag/shadu.pag")
-        // 创建PAGView
-        val pagView = PAGView(this@SavActivity)
-        // 加入ViewGroup
-        pagContainer.addView(pagView)
-        // 设置文件来源
-        pagView.composition = pagFile
-        pagView.setRepeatCount(0)
-        // 播放
-        pagView.play()
     }
 }
--- app/src/main/java/com/example/paglottie/VirActivity.kt
+++ app/src/main/java/com/example/paglottie/VirActivity.kt
@@ -3,25 +3,13 @@ package com.example.paglottie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.FrameLayout
-import org.libpag.PAGFile
-import org.libpag.PAGView
 
 class VirActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_big)
+        setContentView(R.layout.activity_vir)
 
         findViewById<View>(R.id.btn1).setOnClickListener {
-            val pagContainer = findViewById<FrameLayout>(R.id.pagContainer);
-            val pagFile = PAGFile.Load(assets, "vir/pag/shengdian.pag")
-            val pagView = PAGView(this@VirActivity)
-            pagContainer.addView(pagView)
-            pagView.composition = pagFile
-            pagView.setRepeatCount(10000)
-            it.post {
-                pagView.play()
-            }
         }
 
         findViewById<View>(R.id.btn2).setOnClickListener {
@@ -35,18 +23,6 @@ class VirActivity : AppCompatActivity() {
 
 
     fun dynamicAdd() {
-        // ViewGroup
-        val pagContainer = findViewById<FrameLayout>(R.id.pagContainer);
-        // 加载File
-        val pagFile = PAGFile.Load(assets, "vir/pag/shengdian.pag")
-        // 创建PAGView
-        val pagView = PAGView(this@VirActivity)
-        // 加入ViewGroup
-        pagContainer.addView(pagView)
-        // 设置文件来源
-        pagView.composition = pagFile
-        pagView.setRepeatCount(0)
-        // 播放
-        pagView.play()
+
     }
 }
